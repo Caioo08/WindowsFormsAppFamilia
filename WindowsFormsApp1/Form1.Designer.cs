ce0e80ccc6a3545da5778ac14756d5a444038bb9
Usuario <Usuario@CPD02-M12>
2024-07-30 12:15:18 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_renda = new System.Windows.Forms.TextBox();
+            this.txt_energia = new System.Windows.Forms.TextBox();
+            this.txt_tv = new System.Windows.Forms.TextBox();
+            this.txt_agua = new System.Windows.Forms.TextBox();
+            this.txt_alimentacao = new System.Windows.Forms.TextBox();
+            this.txt_outros = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_novo = new System.Windows.Forms.Button();
+            this.btn_fechar = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.lbl_saldo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Renda Familiar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Energia Elétrica";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Água";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "TV/FONE/INTERNET";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Alimentação";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(57, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Outros Gastos";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(59, 354);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total dos Gastos";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(59, 389);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Saldo";
+            // 
+            // txt_renda
+            // 
+            this.txt_renda.Location = new System.Drawing.Point(143, 18);
+            this.txt_renda.Name = "txt_renda";
+            this.txt_renda.Size = new System.Drawing.Size(100, 20);
+            this.txt_renda.TabIndex = 9;
+            // 
+            // txt_energia
+            // 
+            this.txt_energia.Location = new System.Drawing.Point(141, 35);
+            this.txt_energia.Name = "txt_energia";
+            this.txt_energia.Size = new System.Drawing.Size(100, 20);
+            this.txt_energia.TabIndex = 10;
+            // 
+            // txt_tv
+            // 
+            this.txt_tv.Location = new System.Drawing.Point(141, 109);
+            this.txt_tv.Name = "txt_tv";
+            this.txt_tv.Size = new System.Drawing.Size(100, 20);
+            this.txt_tv.TabIndex = 11;
+            // 
+            // txt_agua
+            // 
+            this.txt_agua.Location = new System.Drawing.Point(141, 75);
+            this.txt_agua.Name = "txt_agua";
+            this.txt_agua.Size = new System.Drawing.Size(100, 20);
+            this.txt_agua.TabIndex = 11;
+            // 
+            // txt_alimentacao
+            // 
+            this.txt_alimentacao.Location = new System.Drawing.Point(141, 138);
+            this.txt_alimentacao.Name = "txt_alimentacao";
+            this.txt_alimentacao.Size = new System.Drawing.Size(100, 20);
+            this.txt_alimentacao.TabIndex = 12;
+            // 
+            // txt_outros
+            // 
+            this.txt_outros.Location = new System.Drawing.Point(141, 164);
+            this.txt_outros.Name = "txt_outros";
+            this.txt_outros.Size = new System.Drawing.Size(100, 20);
+            this.txt_outros.TabIndex = 13;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(62, 302);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 14;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_novo
+            // 
+            this.btn_novo.Location = new System.Drawing.Point(160, 302);
+            this.btn_novo.Name = "btn_novo";
+            this.btn_novo.Size = new System.Drawing.Size(115, 23);
+            this.btn_novo.TabIndex = 15;
+            this.btn_novo.Text = "Novo Cálculo";
+            this.btn_novo.UseVisualStyleBackColor = true;
+            this.btn_novo.Click += new System.EventHandler(this.btn_novo_Click);
+            // 
+            // btn_fechar
+            // 
+            this.btn_fechar.Location = new System.Drawing.Point(310, 302);
+            this.btn_fechar.Name = "btn_fechar";
+            this.btn_fechar.Size = new System.Drawing.Size(75, 23);
+            this.btn_fechar.TabIndex = 16;
+            this.btn_fechar.Text = "Fechar";
+            this.btn_fechar.UseVisualStyleBackColor = true;
+            this.btn_fechar.Click += new System.EventHandler(this.btn_fechar_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(202, 353);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(0, 13);
+            this.lbl_resultado.TabIndex = 17;
+            // 
+            // lbl_saldo
+            // 
+            this.lbl_saldo.AutoSize = true;
+            this.lbl_saldo.Location = new System.Drawing.Point(202, 389);
+            this.lbl_saldo.Name = "lbl_saldo";
+            this.lbl_saldo.Size = new System.Drawing.Size(0, 13);
+            this.lbl_saldo.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_agua);
+            this.groupBox1.Controls.Add(this.txt_alimentacao);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_tv);
+            this.groupBox1.Controls.Add(this.txt_energia);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_outros);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(62, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 227);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_saldo);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_fechar);
+            this.Controls.Add(this.btn_novo);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_renda);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_renda;
+        private System.Windows.Forms.TextBox txt_energia;
+        private System.Windows.Forms.TextBox txt_tv;
+        private System.Windows.Forms.TextBox txt_agua;
+        private System.Windows.Forms.TextBox txt_alimentacao;
+        private System.Windows.Forms.TextBox txt_outros;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_novo;
+        private System.Windows.Forms.Button btn_fechar;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Label lbl_saldo;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            double renda = double.Parse(txt_renda.Text);
+            double energia = double.Parse(txt_energia.Text);
+            double agua = double.Parse(txt_agua.Text);
+            double tv = double.Parse(txt_tv.Text);
+            double alimentacao = double.Parse(txt_alimentacao.Text);
+            double outros = double.Parse(txt_outros.Text);
+
+            double resultado = energia+ agua+ tv+ alimentacao+outros ;
+            double saldo = renda - resultado;
+
+            lbl_resultado.Text = resultado.ToString();
+            lbl_saldo.Text = saldo.ToString();
+        }
+
+        private void btn_fechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_novo_Click(object sender, EventArgs e)
+        {
+            txt_renda.Text = "";
+            txt_energia.Text = "";
+            txt_agua.Text = "";
+            txt_tv.Text = "";
+            txt_alimentacao.Text = "";
+            txt_outros.Text = "";
+
+            lbl_resultado.Text = "";
+            lbl_saldo.Text = "";
+        }
+    }
+}
